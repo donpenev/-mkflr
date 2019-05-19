f793119bf6f39e781eed98d5c01e0d04e6f7b0cc
Doncho Penev <b.d.penev@gmail.com>
2019-05-19 18:29:12 +0100

    little work on the navbar strted, added flshes and navbar shared partials to the shared views components folder, added the pngs, necessary for the navbar, continuing work on the devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable  and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+# user_signed_in? => boolean
+# current_user => user instance
