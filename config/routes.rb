8ed7f73dd244a92b74d6a2bc9bee98a8409a4bd4
Doncho Penev <b.d.penev@gmail.com>
2019-06-04 00:51:04 +0100

    finished the cart building but need to make the order actually save, by assigning an account - user or guest

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/patients/:id', to: 'patients#show', as: 'patient' and this in the corresponding view:
+  # <%= link_to 'Patient Record', patient_path(@patient) %>
+
   get 'products', to: 'products#index'
-  resource :cart, only: [:show]
+
   post '/order_items', to: 'order_items#create', as: 'order_items'
+
+  resource :cart, only: [:show]
+
 end
