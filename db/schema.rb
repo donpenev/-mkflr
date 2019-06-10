60714969cc19137088696df9504e22397bfe8536
Doncho Penev <b.d.penev@gmail.com>
2019-06-10 17:35:02 +0100

    simple form in the product cards in index, works in creating order items, created a category in products table, started work on category labels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_122531) do
+ActiveRecord::Schema.define(version: 2019_06_10_163218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_06_05_122531) do
     t.integer "size"
     t.string "webcolour"
     t.string "product_pic"
+    t.string "category"
   end
 
   create_table "tocontacts", force: :cascade do |t|
