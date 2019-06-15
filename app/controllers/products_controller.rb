9bedda557be35cc379b846dc459a9f6cad8323ee
Doncho Penev <b.d.penev@gmail.com>
2019-06-15 01:00:59 +0100

    updated seeds with more content, stareted the cards for the cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @products = Product.all
@@ -7,5 +7,12 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
